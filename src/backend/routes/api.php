59b9a7fe83a89259ae2e79af75c69b2f87836cc2
fadhrusd7 <fadhilrusadi5@gmail.com>
2026-06-02 09:38:27 +0700

    Backup

--- src/backend/routes/api.php
+++ src/backend/routes/api.php
@@ -3,16 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\Auth\AdminAuthController;
-<<<<<<< HEAD
-
-
-// PUBLIC ROUTES
-Route::prefix('auth')->group(function () {
-    Route::post('/register', [AuthController::class, 'register']);
-    Route::post('/login',    [AuthController::class, 'login']);
-});
-
-=======
 use App\Http\Controllers\Auth\GoogleAuthController;
 use App\Http\Controllers\ReportController;
 use App\Http\Controllers\AdminReportController;
@@ -34,29 +24,19 @@ Route::prefix('auth')->group(function () {
 });
 
 // Auth Admin
->>>>>>> 22aa2134171d21cc1c42d8bdc3709927502592cb
 Route::prefix('admin')->group(function () {
     Route::post('/login', [AdminAuthController::class, 'login']);
 });
 
-<<<<<<< HEAD
-// PROTECTED ROUTES
-=======
 // Peta publik (US-02)
 Route::get('/reports/map', [ReportController::class, 'map']);
 
 // USER
->>>>>>> 22aa2134171d21cc1c42d8bdc3709927502592cb
 Route::prefix('auth')->middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/me',      [AuthController::class, 'me']);
 });
 
-<<<<<<< HEAD
-Route::prefix('admin')->middleware('auth:sanctum')->group(function () {
-    Route::post('/logout', [AdminAuthController::class, 'logout']);
-    Route::get('/me',      [AdminAuthController::class, 'me']);
-=======
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/reports',      [ReportController::class, 'store']);
     Route::get('/reports/my',    [ReportController::class, 'myReports']);
@@ -81,5 +61,4 @@ Route::prefix('admin')->middleware(['auth:sanctum', 'is.admin'])->group(function
     Route::patch('/reports/{id}/validate', [AdminReportController::class, 'validate']);
     Route::patch('/reports/{id}/status',   [AdminReportController::class, 'updateStatus']);
     Route::delete('/reports/{id}',         [AdminReportController::class, 'destroy']);
->>>>>>> 22aa2134171d21cc1c42d8bdc3709927502592cb
 });
